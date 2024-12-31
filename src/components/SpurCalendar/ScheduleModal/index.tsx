8d0fac124006d2af9476533c6a397df93fa58d91
XSKW11111 <39919931+XSKW11111@users.noreply.github.com>
2024-12-31 13:53:43 -0800

    fix(spur): fix IconCross missing

--- src/components/SpurCalendar/ScheduleModal/index.tsx
+++ src/components/SpurCalendar/ScheduleModal/index.tsx
@@ -10,6 +10,7 @@ import { createClient } from '@/utils/supabase/client'
 import { useToast } from '@/hooks/use-toast'
 import { cn } from '@/lib/utils';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import IconCross from '@/components/Icons/IconCross';
 
 const ScheduleModal = (): React.ReactElement => {
     const subpabase = createClient();
@@ -76,8 +77,11 @@ const ScheduleModal = (): React.ReactElement => {
                 <Dialog.Portal>
                     <Dialog.Overlay className="fixed inset-0 bg-[#0A0A0A80] bg-opacity-50" />
                     <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg p-6 box-border w-[640px] h-[430px]">
-                        <Dialog.Title className="font-sans text-2xl text-left font-semibold">Schedule Detail</Dialog.Title>
-                        <div className="w-full flex flex-col gap-6">
+                        <div className="w-full flex flex-row items-center justify-between">
+                            <Dialog.Title className="font-sans text-2xl text-left font-semibold">Schedule Detail</Dialog.Title>
+                            <Dialog.Close asChild><div className="cursor-pointer"><IconCross size={16} color="text-black"/></div></Dialog.Close>
+                        </div>
+                        <div className="w-full flex flex-col gap-6 py-4">
                         <fieldset>
                             <label className="block text-sm font-medium text-gray-700">Test Suite</label>
                             <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="mt-1 h-[44px] box-border px-3 py-2block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
@@ -102,10 +106,10 @@ const ScheduleModal = (): React.ReactElement => {
                         </div>
                         </div>
 
-                        <Separator className="w-full my-4 bg-gray-200"/>
+                        <Separator className="w-full bg-gray-200"/>
 
-                        <div className="w-full flex flex-row gap-2 items-center justify-end mt-4">
-                        <div className="w-full flex flex-row gap-2 items-center justify-start">
+                        <div className="w-full flex flex-row gap-2 items-center justify-center">
+                        <div className="w-full flex flex-row gap-2 items-center justify-start py-4 box-border">
                         <Dialog.Close asChild>
                             <button className="grow w-full bg-white hover:bg-gray-200 border border-gray-200 box-border flex flex-row gap-2 items-center justify-center py-2 px-3 rounded-lg">
                                 <p className="text-red-600 font-semibold text-sm">Cancel Schedule</p>
